c41412b809d3f1ba9d1e57b3c81c8b427b0ce193
OnahGodwin <88001517+GodwinOnah@users.noreply.github.com>
2022-07-24 21:35:08 +0100

    Created a new git repo for Server

---
+++ node/app.js
@@ -0,0 +1,157 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const bcrypt=require('bcrypt-nodejs');
+const cors=require('cors');
+
+
+const app=express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+
+const myDatabese={
+		users:[
+		{
+				id: '1',
+				name: 'Godwin',
+				email: 'obande@gmail.com',
+				password:'1234',
+				entries:0,
+				joined: new Date()
+
+		},
+
+		{
+			
+				id: '2',
+				name: 'Sam',
+				email: 'sam@gmail.com',
+				password:'4321',
+				entries:0,
+				joined: new Date()
+
+		}
+
+
+]}
+
+app.get('/',(req,res)=>{
+
+		return res.send(myDatabese.users);
+})
+
+
+
+
+
+
+
+
+app.get('/loginpage',(req,res)=>{//logging in a user
+
+	if(req.body.email===myDatabese.users[0].email&&
+
+		req.body.password===myDatabese.users[0].password
+		){
+
+			res.json('success');
+
+
+	}
+
+		else{
+				res.status(404).json('error logging in');
+
+		}
+})
+
+
+
+
+
+
+
+
+
+app.post('/singuppage',(req,res)=>{//registering a user
+
+	// bcrypt.hash(password,null,null,function(err,hash)){
+
+
+	// }
+
+		myDatabese.users.push({
+				id: '3',
+				name: req.body.name,
+				email: req.body.email,
+				password:req.body.password,
+				entries:0,
+				joined: new Date()
+
+		})
+})
+
+
+
+
+
+
+app.get('/profile/:id',(req,res)=>{//using Id to fetch user
+
+		let found =false;
+	myDatabese.users.forEach(user=>{
+
+		if(user.id===req.params.id){
+
+			found=true;
+
+			return res.json(user);
+
+		}
+		if(!found){
+
+			res.status(404).json('no such user exist');
+		}
+	})
+
+		
+});
+
+
+
+app.post('/image',(req,res)=>{//Calculating entries after adding image
+
+		let found =false;
+	myDatabese.users.forEach(user=>{
+
+		if(user.id===req.params.id){
+
+			found=true;
+			user.entries++;
+
+			return res.json(user.entries);
+
+		}
+		if(!found){
+
+			res.status(404).json('no such user exist');
+		}
+	})
+
+		})
+
+	
+
+
+
+
+
+
+app.listen(3001,function(){
+
+console.log("Sever running at port: 3001");
+
+});
---
+++ node/carImageList.js
@@ -0,0 +1,13 @@
+import {car1,car2,car3,car4,car5,car6,car7,car8,car9,
+	car10,car11,car12,car13,car14,car15,bmw1,bmw2,bmw3,
+	bmw4,bmw5,bmw6,bus,bus1,bus2,bus3,bus4,bus5,bus6,bus7
+	,Cadilac2,Cadilac3,Cadilac4,Ferrari1,Ferrari2,Ferrari3,
+	Jeep1,Jeep2,Jeep3,Jeep4,Jeep5} from './carImageUrl'
+
+
+	export const carImagesList=[car1,car2,car3,car4,car5,car6,car7,car8,car9,
+						car10,car11,car12,car13,car14,car15,bmw1,bmw2,bmw3,
+						bmw4,bmw5,bmw6,bus,bus1,bus2,bus3,bus4,bus5,bus6,bus7
+						,Cadilac2,Cadilac3,Cadilac4,Ferrari1,Ferrari2,Ferrari3,
+							Jeep1,Jeep2,Jeep3,Jeep4,Jeep5];
+
