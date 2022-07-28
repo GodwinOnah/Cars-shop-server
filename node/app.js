96170222b8a8443368ea45314ddea7a0095f2f25
OnahGodwin <88001517+GodwinOnah@users.noreply.github.com>
2022-07-28 04:34:37 +0100

    Added signup, fetch from database, database connection using knex, connecting database to nodejs

--- node/app.js
+++ node/app.js
@@ -2,6 +2,28 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const bcrypt=require('bcrypt-nodejs');
 const cors=require('cors');
+const knex=require('knex');
+
+
+
+
+const postgres=knex({//connecting to database using knex
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 3306,
+    user : 'Godwin',
+    password : '',
+    database : 'CarsDatabase'
+  }
+});
+
+
+postgres.select('*').from('CarsDatabase').then(data=>{//selecting from database
+		console.log(data);
+
+})
+
 
 
 const app=express();
@@ -12,31 +34,34 @@ app.use(cors());
 
 
 
-const myDatabese={
-		users:[
-		{
-				id: '1',
-				name: 'Godwin',
-				email: 'obande@gmail.com',
-				password:'1234',
-				entries:0,
-				joined: new Date()
+// const myDatabese={
+// 		users:[
+// 		{
+// 				id: '1',
+// 				name: 'Godwin',
+// 				email: 'obande@gmail.com',
+// 				password:'1234',
+// 				entries:0,
+// 				joined: new Date()
 
-		},
+// 		},
 
-		{
+// 		{
 			
-				id: '2',
-				name: 'Sam',
-				email: 'sam@gmail.com',
-				password:'4321',
-				entries:0,
-				joined: new Date()
+// 				id: '2',
+// 				name: 'Sam',
+// 				email: 'sam@gmail.com',
+// 				password:'4321',
+// 				entries:0,
+// 				joined: new Date()
 
-		}
+// 		}
 
 
-]}
+// ]}
+
+
+
 
 app.get('/',(req,res)=>{
 
@@ -76,22 +101,38 @@ app.post('/login',(req,res)=>{//logging in a user
 
 
 
-app.post('/singu',(req,res)=>{//registering a user
+app.post('/singup',(req,res)=>{//registering a user
 
-	// bcrypt.hash(password,null,null,function(err,hash)){
+	const hash=bcrypt.hashsync(password);
 
+	db.transaction(trx=>{
 
-	// }
+		trx.insert({
 
-		myDatabese.users.push({
-				id: '3',
-				name: req.body.name,
-				email: req.body.email,
-				password:req.body.password,
-				entries:0,
-				joined: new Date()
+			hash:hash,
+			email:email  })
 
-		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail=>{
+
+		return db('CarsDatabase')
+				.returnning().
+
+				insert(
+						id: '3',
+						name: req.body.name,
+						email: req.body.email,
+						password:req.body.password,
+						entries:0,
+						joined: new Date()
+
+				).then(user=>{
+
+			res.json(user[0]);
+		
+		}).catch(err=>res.status(400).json('Not registered'))
+
 })
 
 
@@ -101,47 +142,73 @@ app.post('/singu',(req,res)=>{//registering a user
 
 app.get('/profile/:id',(req,res)=>{//using Id to fetch user
 
-		let found =false;
-	myDatabese.users.forEach(user=>{
+		db.select('*').from('CarsDatabase')
+		.where({req.params.id})
+		.then(user=>{
 
-		if(user.id===req.params.id){
+			if(user.length){
+			res.json(user[0])
+				}else{
+						res.status(400).json('Not found')
 
-			found=true;
+				}
 
-			return res.json(user);
+		}).catch(err=>res.status(400).json('erro getting user'))
 
-		}
-		if(!found){
-
-			res.status(404).json('no such user exist');
-		}
 	})
 
+
+
+
+
+
+
+
+	// myDatabese.users.forEach(user=>{
+
+	// 	if(user.id===req.params.id){
+
+	// 		found=true;
+
+	// 		return res.json(user);
+
 		
-});
+
+		
 
 
 
 app.post('/image',(req,res)=>{//Calculating entries after adding image
 
-		let found =false;
-	myDatabese.users.forEach(user=>{
 
-		if(user.id===req.params.id){
 
-			found=true;
-			user.entries++;
+	db('CarsDatabase').where('id','=',req.body.id)
+	.increament('entries',1)
+	.returning('enteries')
+	.then(entries=>{
 
-			return res.json(user.entries);
-
-		}
-		if(!found){
-
-			res.status(404).json('no such user exist');
-		}
-	})
-
+			res.json(entries[0]);
+	}).catch(err=>res.status(400).json('erro getting user'))
 		})
+
+
+
+
+		
+	// myDatabese.users.forEach(user=>{
+
+	// 	if(user.id===req.params.id){
+
+	// 		found=true;
+	// 		user.entries++;
+
+	// 		return res.json(user.entries);
+
+	// 	}
+		
+	
+
+		
 
 	
 
